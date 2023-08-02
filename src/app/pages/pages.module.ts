a34e959e8e2bd6460049dc63b1d10490cd0c68b4
angelmorales1 <angelmorales0815@gmail.com>
2023-08-01 21:17:42 -0300

    feat: register form completed

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { HomeComponent } from './home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { MaterialModule } from '../material/material.module';
 import { ComponentsModule } from '../components/components.module';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
-    HomeComponent
+    LoginComponent
   ],
   imports: [
     CommonModule,
     MaterialModule,
+    ReactiveFormsModule,
     ComponentsModule,
     NgOptimizedImage,
   ]
